7354d1fc7fbb9be33db5fc27f14f92fd1238631b
Kaway <philippekaway@gmail.com>
2026-08-04 18:04:20 -0300

    fix: update header file

--- ft_printf.h
+++ ft_printf.h
@@ -3,16 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phkaway- <phkaway-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:19:44 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/07/09 15:47:26 by phkaway-         ###   ########.fr       */
+/*   Updated: 2026/08/04 18:00:01 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
+#include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+int ft_printf(const char *, ...);
+int	ft_putstr(char *str);
+int	ft_putchar(char c);
+
 
 #endif
