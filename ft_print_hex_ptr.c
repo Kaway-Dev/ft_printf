17837cf1b76843317888d41b26b524cbe3b28fc4
Kaway <philippekaway@gmail.com>
2026-08-05 12:02:29 -0300

    feat: implement ft_puthex_ulong and ft_putptr functions for hexadecimal and pointer printing

--- ft_print_hex_ptr.c
+++ ft_print_hex_ptr.c
@@ -3,11 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_hex_ptr.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phkaway- <phkaway-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:36:20 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/07/09 15:36:22 by phkaway-         ###   ########.fr       */
+/*   Updated: 2026/08/05 12:01:40 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+static int	ft_puthex_ulong(unsigned long n)
+{
+    int count;
+
+    count = 0;
+    if (n >= 16)
+        count += ft_puthex_ulong(n / 16);
+    count += ft_putchar("0123456789abcdef"[n % 16]);
+    return(count);
+}
+
+int			ft_putptr(void *ptr)
+{
+    
+}
+
+
+int	ft_puthex(unsigned int n, char *base)
+{
+    int count;
+
+    count = 0;
+    if (n >= 16)
+        count += ft_puthex(n / 16, base);
+    count += ft_putchar(base[n % 16]);
+    return (count);
+}
