d25b9e8d0647121f326d3b6c5756e2e80c0b65fd
Kaway <philippekaway@gmail.com>
2026-08-05 15:33:54 -0300

    fix: update header file formatting and remove unnecessary includes

--- ft_printf.h
+++ ft_printf.h
@@ -6,23 +6,21 @@
 /*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:19:44 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/08/05 12:15:53 by philippe         ###   ########.fr       */
+/*   Updated: 2026/08/05 15:33:21 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
 
-int ft_printf(const char *, ...);
+int	ft_printf(const char *fmt, ...);
 int	ft_putstr(char *str);
 int	ft_putchar(char c);
-int ft_putnbr(int n);
+int	ft_putnbr(int n);
 int	ft_put_unsigned(unsigned int n);
 int	ft_puthex(unsigned int n, char *base);
-int			ft_putptr(void *ptr);
-
+int	ft_putptr(void *ptr);
 
 #endif
