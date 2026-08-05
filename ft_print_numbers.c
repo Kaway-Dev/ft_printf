576120c259dd7f9f52f99709cf5c0b58d9f6eda0
Kaway <philippekaway@gmail.com>
2026-08-04 23:43:09 -0300

    feat: implement ft_putnbr and ft_put_unsigned functions for number printing

--- ft_print_numbers.c
+++ ft_print_numbers.c
@@ -3,11 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phkaway- <phkaway-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:36:33 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/07/09 15:36:34 by phkaway-         ###   ########.fr       */
+/*   Updated: 2026/08/04 23:40:17 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+int ft_putnbr(int n)
+{
+    long    nb;
+    int     count;
+
+    count = 0;
+    nb = n;
+    if (nb < 0)
+    {
+        count += ft_putchar('-');
+        nb = -nb;
+    }
+    if (nb >= 10)
+        count += ft_putnbr (nb / 10);
+    count += ft_putchar (nb % 10 + '0');
+    return (count);
+}
+
+int	ft_put_unsigned(unsigned int n)
+{
+    int count;
+
+    count = 0;
+    if (n >= 10)
+        count += ft_put_unsigned (n / 10);
+    count += ft_putchar (n % 10 + '0');
+    return (count);
+}
