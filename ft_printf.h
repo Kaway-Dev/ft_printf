2b9a156f4f42b742d1d2760aaf92c888ded080f8
Kaway <philippekaway@gmail.com>
2026-08-05 12:03:31 -0300

    feat:  add function prototypes for ft_puthex, ft_putptr, and ft_puthex_ulong

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:19:44 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/08/04 23:42:18 by philippe         ###   ########.fr       */
+/*   Updated: 2026/08/05 11:50:55 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int	ft_putstr(char *str);
 int	ft_putchar(char c);
 int ft_putnbr(int n);
 int	ft_put_unsigned(unsigned int n);
+int	ft_puthex(unsigned int n, char *base);
+int			ft_putptr(void *ptr);
+static int	ft_puthex_ulong(unsigned long n);
 
 
 #endif
