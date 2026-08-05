5080984940e3e903abefbb170c55be2f4e8888d3
Kaway <philippekaway@gmail.com>
2026-08-05 15:49:21 -0300

    fix: norma

--- ft_print_hex_ptr.c
+++ ft_print_hex_ptr.c
@@ -6,7 +6,7 @@
 /*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:36:20 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/08/05 12:19:31 by philippe         ###   ########.fr       */
+/*   Updated: 2026/08/05 15:46:49 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,34 @@
 
 static int	ft_puthex_ulong(unsigned long n)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    if (n >= 16)
-        count += ft_puthex_ulong(n / 16);
-    count += ft_putchar("0123456789abcdef"[n % 16]);
-    return(count);
+	count = 0;
+	if (n >= 16)
+		count += ft_puthex_ulong(n / 16);
+	count += ft_putchar("0123456789abcdef"[n % 16]);
+	return (count);
 }
 
-int			ft_putptr(void *ptr)
+int	ft_putptr(void *ptr)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    if (ptr == NULL)
-        return (ft_putstr("(nil)"));
-    count += ft_putstr("0x");
-    count += ft_puthex_ulong((unsigned long)ptr);
-    return(count);
+	count = 0;
+	if (ptr == NULL)
+		return (ft_putstr("(nil)"));
+	count += ft_putstr("0x");
+	count += ft_puthex_ulong((unsigned long)ptr);
+	return (count);
 }
-
 
 int	ft_puthex(unsigned int n, char *base)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    if (n >= 16)
-        count += ft_puthex(n / 16, base);
-    count += ft_putchar(base[n % 16]);
-    return (count);
+	count = 0;
+	if (n >= 16)
+		count += ft_puthex(n / 16, base);
+	count += ft_putchar(base[n % 16]);
+	return (count);
 }
--- ft_print_numbers.c
+++ ft_print_numbers.c
@@ -6,37 +6,37 @@
 /*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 15:36:33 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/08/04 23:40:17 by philippe         ###   ########.fr       */
+/*   Updated: 2026/08/05 15:47:56 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putnbr(int n)
+int	ft_putnbr(int n)
 {
-    long    nb;
-    int     count;
+	long	nb;
+	int		count;
 
-    count = 0;
-    nb = n;
-    if (nb < 0)
-    {
-        count += ft_putchar('-');
-        nb = -nb;
-    }
-    if (nb >= 10)
-        count += ft_putnbr (nb / 10);
-    count += ft_putchar (nb % 10 + '0');
-    return (count);
+	count = 0;
+	nb = n;
+	if (nb < 0)
+	{
+		count += ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		count += ft_putnbr (nb / 10);
+	count += ft_putchar (nb % 10 + '0');
+	return (count);
 }
 
 int	ft_put_unsigned(unsigned int n)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    if (n >= 10)
-        count += ft_put_unsigned (n / 10);
-    count += ft_putchar (n % 10 + '0');
-    return (count);
+	count = 0;
+	if (n >= 10)
+		count += ft_put_unsigned (n / 10);
+	count += ft_putchar (n % 10 + '0');
+	return (count);
 }
